92d45c3106f93f5e31d148765a8013ef9f26cee8
salimp2009@gmail.com <salimp2009@gmail.com>
2023-05-13 17:40:52 +0300

    started updating Pagination numbers and buttons visibility in Pagination.js with a render function

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -9,12 +9,24 @@ import {
 
 import renderJobList from "./JobList.js";
 
+const renderPaginationBtns = () => {
+  if (state.currentPage >= 2) {
+    paginationBtnBackEl.classList.remove("pagination__button--hidden");
+    paginationNumberNextEl.textContent = +state.currentPage;
+    paginationNumberBackEl.textContent = +(state.currentPage - 1);
+  } else {
+    paginationBtnBackEl.classList.add("pagination__button--hidden");
+  }
+};
+
 const clickHandler = (event) => {
   const clickedButtonEl = event.target.closest(".pagination__button");
   if (!clickedButtonEl) return;
 
   const nextPage = clickedButtonEl.className.includes("--next") ? true : false;
   nextPage ? state.currentPage++ : state.currentPage--;
+
+  renderPaginationBtns();
   renderJobList();
 };
 
